577d6e578f1d963615c8a82ba8e8bffcc9f131a6
Spencer Blum <spencerblum@gmail.com>
2020-01-15 23:43:55 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -13,5 +13,4 @@ def meal_choice(veg1, veg2, protein="meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 
-  return meal # return here is optional. Ruby methods automatically return the last expression
 end
